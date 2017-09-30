b6120d08573c66b83fed9bb7a83f052e6a9801f0
KristianAlves <kristian.alves@gmail.com>
2017-09-30 13:18:05 +0000

    installing Capistrano

--- db/migrate/20170712000917_add_category_to_products.rb
+++ db/migrate/20170712000917_add_category_to_products.rb
@@ -1,4 +1,4 @@
-class AddCategoryToProducts < ActiveRecord::Migration
+                                                                                                                                                                                            class AddCategoryToProducts < ActiveRecord::Migration
   def change
     add_reference :products, :category, index: true, foreign_key: true
   end
