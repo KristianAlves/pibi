79ed60f67be9c75dca8bc2051a65a5257fb14e81
KristianAlves <kristian.alves@gmail.com>
2017-10-03 23:14:12 +0000

    installing Capistrano

--- db/migrate/20170516223346_create_products.rb
+++ db/migrate/20170516223346_create_products.rb
@@ -3,8 +3,9 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.references :brand, index: true, foreign_key: true
-      t.references :admin, index: true, foreign_key: true
+      #t.references :subcategory, index: true, foreign_key: true
+      #t.references :brand, index: true, foreign_key: true
+      #t.references :admin, index: true, foreign_key: true
 
       t.timestamps null: false
     end
