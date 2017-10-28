c590e1e64e39dcd8b884bc94e6ec41a0c1d0e9ec
KristianAlves <kristian.alves@gmail.com>
2017-10-28 09:49:48 -0200

    Fix Search

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -3,6 +3,8 @@ class Site::SearchController < SiteController
     @products = Product.search(params[:q], params[:page])
     @categories = Category.all
     @carts_itens = Cart.count
+    @logos = Logo.all
+    @brands = Brand.all
     @carts_consumer_itens = CartConsumer.count
   end
 end
