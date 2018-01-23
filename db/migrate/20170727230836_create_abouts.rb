8a9e911806ac2f64c6d85d07e4d6d44dfdd88891
Kristian Alves <kristian.alves@gmail.com>
2018-01-23 23:16:19 +0000

    Fix the limit of the description in About

--- db/migrate/20170727230836_create_abouts.rb
+++ db/migrate/20170727230836_create_abouts.rb
@@ -2,7 +2,7 @@ class CreateAbouts < ActiveRecord::Migration
   def change
     create_table :abouts do |t|
       t.string :title
-      t.string :description
+      t.text :description
 
       t.timestamps null: false
     end
