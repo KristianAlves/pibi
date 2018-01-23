7270ffaf1319de43526f24e423bdd149996d200d
Kristian Alves <kristian.alves@gmail.com>
2018-01-23 23:57:04 +0000

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,13 +2,13 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 # Devise Config
-  config.action_mailer.default_url_options = { host: 'kristian.com.br' }
+  config.action_mailer.default_url_options = { host: 'comercialpibi.com.br' }
 
   # Mailgun
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: Rails.application.secrets.mailgun_secret_api_key,
-    domain: 'mail.kristian.com.br',
+    domain: 'mail.comercialpibi.com.br',
   }
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 20170808231053) do
 
   create_table "abouts", force: :cascade do |t|
     t.string   "title"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "description", limit: 9
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
   create_table "admins", force: :cascade do |t|
